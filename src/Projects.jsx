0274f551577fbe122ae90fd6377e8d9d5199e84e
Juan Castaneda <jccastanedacamargo@outlook.com>
2025-07-31 12:24:42 +0200

    setup env file fetchprojects and update components

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,7 +1,35 @@
-import './fetchProjects';
+import { useFetchProject } from './fetchProjects';
 
 const Projects = () => {
-  return <h2>Projects</h2>;
+  const [loading, projects] = useFetchProject();
+
+  if (loading) {
+    return (
+      <section className="projects">
+        <h2>Loading...</h2>;
+      </section>
+    );
+  }
+
+  return (
+    <section className="projects">
+      <div className="title">
+        <h2>projects</h2>
+        <div className="title-underline"></div>
+      </div>
+      <div className="projects-center">
+        {projects.map((project) => {
+          const { id, img, title, url } = project;
+          return (
+            <a href={url} key={id} className="project" target="_blank" rel="noreferrer">
+              <img src={img} alt={title} className="img" />
+              <h5>{title}</h5>
+            </a>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
